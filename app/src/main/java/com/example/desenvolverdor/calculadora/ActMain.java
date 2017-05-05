9dd16dd03d706abe3bf80613dd8439439c939322
Vitório <s.vitorio.alves@gmail.com>
2017-05-05 19:09:27 -0300

    Begin

---
+++ app/src/main/java/com/example/desenvolverdor/calculadora/ActMain.java
@@ -0,0 +1,13 @@
+package com.example.desenvolverdor.calculadora;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_main);
+    }
+}
